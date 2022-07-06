afd4881592b134475deadd8d01a2607a6b940116
Prathamesh Mathapati <93044026+prathamesh-mathapati@users.noreply.github.com>
2022-07-06 09:49:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,58 +1,37 @@
-import React from 'react';
-import '../src/index.css'
-import data from './data'
-
+import './App.css';
+// import { Footer } from './component/Footer';
+import { Navbar } from './component/Navbar';
+import { Routes, Route,  } from "react-router-dom";
+import { PopulerSearch } from "./component/PopulerSearch";
+import { TopRatedMovie } from './component/TopRatedMovie';
+import { PopulerMoviesDetails } from './component/PopulerMoviesDetails';
+import { TopRatedMovieDetails } from './component/TopRatedMovieDetails';
+// import { PureMain } from './component/PureMain';
+// import {Footer} from './component/Footer'
+import { DefaultMovieMovieDetails } from './component/DefaultMovieMovieDetails';
 
 
 
 function App() {
   return (
-    <div className="grid-container">
-    <header className="row">
-        <div>
-            <a className="brand" href="">amazona</a>
-        </div>
-        <div>
-            <a href="/cart">Cart</a>
-            <a href="/singin">Sing In</a>
-        </div>
-
-    </header>
-    <main>
-        <div className="row center">
-          {
-            data.products.map(products=>(
-
-              <div key={products._id} className="card">
-                  <a href={`/product/${products._id}`}>
-                      <img  className="medium" src={`${products.image}`} alt=""/>
-                  </a>
-  
-                  <div className="card-body">
-                  <a href={`/product/${products._id}`}>
-                          <h2>{products.name}</h2>
-                      </a>
-                      <div className="rating">
-                          <span><i className="fa fa-star"></i></span>
-                          <span> <i className="fa fa-star"></i></span>
-                          <span><i className="fa fa-star"></i></span>
-                          <span><i className="fa fa-star"></i></span>
-                          <span><i className="fa fa-star"></i></span>
-  
-                      </div>
-                  <div className="price">${products.price}</div>
-                  </div>
-              </div>
-
-  ))  
-          }
-        </div>
-    </main>
-
-    <footer className="row center">
-        All rin
-    </footer>
-</div>
+    <div className="App">
+      {/* <PureMain/> */}
+      <Routes>
+        <Route path="/" element={ <Navbar/>}>
+        </Route>
+        <Route path="/TopRatedMovie" element={ <TopRatedMovie/>}>
+        </Route>
+        <Route path="/PopulerSearch" element={ <PopulerSearch/>}>  
+        </Route>
+        <Route path="/PopulerMoviesDetails"  element={<PopulerMoviesDetails/>}>  
+        </Route>
+        <Route path="/TopRatedMovieDetails"  element={<TopRatedMovieDetails/>}>  
+        </Route>
+        <Route path="/DefaultMovieMovieDetails"  element={<DefaultMovieMovieDetails/>}>  
+        </Route>
+      </Routes>
+      {/* <Footer/> */}
+    </div>
   );
 }
 
