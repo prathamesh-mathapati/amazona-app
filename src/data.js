755e4597799234bddd4c54a92a3ee02bd6611d32
Prathamesh Mathapati <93044026+prathamesh-mathapati@users.noreply.github.com>
2022-07-06 09:17:09 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,73 @@
+const data={
+    products:[
+        {
+            _id:1,
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'./images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviwes:10,
+            description:'High quality product'
+        },
+        {
+            _id:2,
+            name:'Adidas Slim Shirt',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviwes:10,
+            description:'High quality product'
+        },
+        {
+            _id:3,
+            name:'Lacoste Free Shirt',
+            category:'Shirts',
+            image:'./images/p3.jpg',
+            price:220,
+            brand:'Lacoste',
+            rating:4.5,
+            numReviwes:10,
+            description:'High quality product'
+        },
+        {
+            _id:4,
+            name:'Nike Slim Shirt',
+            category:'PAnts',
+            image:'./images/p4.jpg',
+            price:78,
+            brand:'Nike',
+            rating:4.5,
+            numReviwes:14,
+            description:'High quality product'
+        },
+        {
+            _id:5,
+            name:'Puma Slim Shirt',
+            category:'Pants',
+            image:'./images/p5.jpg',
+            price:120,
+            brand:'Puma',
+            rating:4.5,
+            numReviwes:10,
+            description:'High quality product'
+        },
+        {
+            _id:6,
+            name:'Adidas Slim Shirt',
+            category:'Pants',
+            image:'./images/p6.jpg',
+            price:139,
+            brand:'Adidas',
+            rating:4.5,
+            numReviwes:10,
+            description:'High quality product'
+        },
+        
+    ]
+}
+
+export default data;
